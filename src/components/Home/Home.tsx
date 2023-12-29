4fd41273283ecc1e517f5748d3d5cfd2e212ebba
Pavel Piasetski <pavel.piasetski@sugarcrm.com>
2023-12-29 19:24:49 +0300

    CC-36 Create NewsPage block on HomePage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,64 +2,69 @@ import React from 'react'
 import {Button, Carousel, ConfigProvider} from 'antd';
 
 import tmpImage from '../../assets/images/measuring_progress-750x375.jpg';
+import tmpImage2 from '../../assets/images/in_progress_v.jpg';
 import styled from "styled-components";
 import {MainLable} from "./MainLable";
+import {NewsBlock} from "./NewsBlock/NewsBlock";
 
 type HomeProps = {}
 
 const ScheduleButton = styled(Button)`
-  width: fit-content;
-  height: 4.5vh;
-  
-  color: white;
-  border-radius: 15px;
+    width: fit-content;
+    height: 4.5vh;
 
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
+    color: white;
+    border-radius: 15px;
 
-  position: absolute;
-  left: 1.4%;
-  bottom: 4%;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
 
-  font-family: 'Oswald', serif;
-  font-weight: 1000;
-  font-size: 1.5vh;
-  text-align: center;
+    position: absolute;
+    left: 1.4%;
+    bottom: 4%;
+
+    font-family: 'Oswald', serif;
+    font-weight: 1000;
+    font-size: 1.5vh;
+    text-align: center;
 `
 
 const CarouselWrapper = styled.div`
-  position: relative;
-  margin: 0;
-  padding: 0;
+    position: relative;
+    margin: 0;
+    padding: 0;
 `;
 
 const HomeImage = styled.img`
-  max-height: 100vh;
-  object-fit: cover;
+    max-height: 100vh;
+    object-fit: cover;
 `;
 
 
 export const Home: React.FC<HomeProps> = () => {
     return (
-        <CarouselWrapper>
-            <MainLable></MainLable>
-            <Carousel autoplay>
-                <HomeImage src={tmpImage}/>
-                <HomeImage src={tmpImage}/>
-                <HomeImage src={tmpImage}/>
-                <HomeImage src={tmpImage}/>
-            </Carousel>
-            <ConfigProvider
-                theme={{
-                    token: {
-                        colorPrimary: '#F0A202',
-                    },
-                }}
-            >
-                <ScheduleButton type="primary">Расписание богослужений</ScheduleButton>
-            </ConfigProvider>
+        <>
+            <CarouselWrapper>
+                <MainLable></MainLable>
+                <Carousel autoplay>
+                    <HomeImage src={tmpImage}/>
+                    <HomeImage src={tmpImage}/>
+                    <HomeImage src={tmpImage}/>
+                    <HomeImage src={tmpImage2}/>
+                </Carousel>
+                <ConfigProvider
+                    theme={{
+                        token: {
+                            colorPrimary: '#F0A202',
+                        },
+                    }}
+                >
+                    <ScheduleButton type="primary">Расписание богослужений</ScheduleButton>
+                </ConfigProvider>
 
-        </CarouselWrapper>
+            </CarouselWrapper>
+            <NewsBlock/>
+        </>
     )
 }
