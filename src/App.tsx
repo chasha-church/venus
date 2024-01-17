283d493646df3991d4c1138cbeab37ef6887dd07
Sergey <serzh.kachalyuk@gmail.com>
2024-01-18 00:25:49 +0300

    CC-36 Added Content component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import styled, {ThemeProvider} from "styled-components"
 import { GlobalStyles } from './styles/Global'
 import {theme} from "./styles/Theme";
-import {Home} from "./components/Content/Home/Home";
+import { Content } from "./components/Content/Content";
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyles />
             <div className="App">
-                <Home />
+                <Content />
             </div>
         </ThemeProvider>
     )
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Home } from './Home/Home'
+
+type ContentProps = {}
+
+export const Content: React.FC<ContentProps> = () => {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
