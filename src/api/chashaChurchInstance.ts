6372d48fc091c70ce4ccf7168b747f38708c56c6
Sergey <serzh.kachalyuk@gmail.com>
2024-01-31 19:10:40 +0300

    CC-51 Remove withCredentials property

--- src/api/chashaChurchInstance.ts
+++ src/api/chashaChurchInstance.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-export const chashaChurchInstance = axios.create({
-    withCredentials: true,
+export const chashaChurchInstance = axios.create({  
     baseURL: "https://cc-neptune-39e02856f5ff.herokuapp.com/api/v1"
 });
