6773aa94ffab4a1ce71675a7976dedfe2a5071b4
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2024-03-29 10:34:31 +0300

    CC-56 [FE] Replace PNG pictures into SVG in whole app (#13)
    * CC-61 Make PreviewBlock more suitable for all image sizes
    
    * CC-56 [FE] Replace PNG pictures into SVG in whole app
    
    * Fixes and improvements due to dev feedback
    
    * UI fixes, icon improvements
    
    * Code style fixes

--- src/components/Sidebar/SidebarHeader/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader/SidebarHeader.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
-
-const ChaliceIcon = './images/ChaliceIcon.png'
+import { ChashaIcon } from '../../common/Icons/ChashaIcon';
 
 type SidebarHeaderProps = {
     expanded: boolean;
     setExpanded: () => void;
 }
 
-export const SidebarHeader: React.FC<SidebarHeaderProps> = ({ expanded, setExpanded }) => {
+export const SidebarHeader: React.FC<SidebarHeaderProps> = ({ expanded }) => {
 
     return (
-        <div className={`flex items-center pl-4 pt-4 ${expanded ? "pr-8" : "pr-4"}`}>
-            <img src={ChaliceIcon} className="w-12 cursor-pointer" alt="Logo" />
+        <div className={`flex items-center ${expanded ? "" : "justify-center"}`}>
+            <ChashaIcon width={68} height={92} />
             <span
-                className={`overflow-hidden font-bold text-gray-600 transition-all ${expanded ? "ml-2" : "w-0 ml-0"}`}>
+                className={`overflow-hidden font-bold text-gray-600 transition-all mt-2 ${expanded ? "" : "w-0 ml-0"}`}>
                 Неупиваемая Чаша
             </span>
         </div>
